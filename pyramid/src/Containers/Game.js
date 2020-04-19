6395619a857ae477a9f542aeaada5fb657148e92
Luis Ramirez <luis.ramirez.12@sansano.usm.cl>
2020-04-19 16:40:01 -0400

    Set structure

--- pyramid/src/Containers/Game.js
+++ pyramid/src/Containers/Game.js
@@ -7,7 +7,8 @@ class Game extends Component{
         this.state = {
             users: [],
             pyramid_height: 1,
-            number_users: 1
+            number_users: 1,
+            structure: []
         }
     }
     
@@ -15,12 +16,22 @@ class Game extends Component{
         this.setState({users: JSON.parse(localStorage.getItem("users"))});
         this.setState({pyramid_height: JSON.parse(localStorage.getItem("pyramid_height"))});
         this.setState({number_users : JSON.parse(localStorage.getItem("number_users"))});
+        this.setStructure(JSON.parse(localStorage.getItem("pyramid_height")));
+    }
+
+    setStructure(pyramid_height){
+        const height = pyramid_height;
+        const structure_array = []
+        for (let index = height; index > 0; index--) {
+            const data = new Array(index).fill("X")
+            structure_array.push(data)
+        }
+        this.setState({structure: structure_array})
     }
 
     render(){
         return(
             <div>
-                <p>{this.state.number_users}</p>
                 <p>{this.state.pyramid_height}</p>
             </div>
         );
