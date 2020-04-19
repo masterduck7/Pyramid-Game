d4b84804a002aabb8d7903d69a1da1a7bebe7ebe
Luis Ramirez <luis.ramirez.12@sansano.usm.cl>
2020-04-19 16:23:56 -0400

    Data obtained from local storage and set the variables

--- pyramid/src/Containers/Game.js
+++ pyramid/src/Containers/Game.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 
 class Game extends Component{
+
+    constructor(state){
+        super(state)
+        this.state = {
+            users: [],
+            pyramid_height: 1,
+            number_users: 1
+        }
+    }
+    
+    componentDidMount(){
+        this.setState({users: JSON.parse(localStorage.getItem("users"))});
+        this.setState({pyramid_height: JSON.parse(localStorage.getItem("pyramid_height"))});
+        this.setState({number_users : JSON.parse(localStorage.getItem("number_users"))});
+    }
+
     render(){
         return(
-            <p>s</p>
+            <div>
+                <p>{this.state.number_users}</p>
+                <p>{this.state.pyramid_height}</p>
+            </div>
         );
     }
 }
